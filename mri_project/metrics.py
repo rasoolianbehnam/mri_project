22051b80b4c3d26ac60f5d0021958566e5dcf8a7
rasoolianbehnam <behnam.rasoulian@gmail.com>
2020-09-08 06:31:47 -0500

    added jupyter notebooks

--- mri_project/metrics.py
+++ mri_project/metrics.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+import joblib
 import numpy as np
 
 all_stats = []
@@ -30,14 +33,34 @@ def total_accuracy(pred, orig):
     return np.sum(pred == orig) / total_pixels
 
 
+def true_positive(pred, orig):
+    return np.sum((pred > 0) & (orig > 0))
+
+
+def true_negative(pred, orig):
+    return np.sum((pred == 0) & (orig == 0))
+
+
+def false_positive(pred, orig):
+    return np.sum((pred > 0) & (orig == 0))
+
+
+def false_negative(pred, orig):
+    return np.sum((pred == 0) & (orig > 0))
+
+
 @stat
 def accuracy(pred, orig):
     uniques = set(np.unique(pred)) | set(np.unique(orig))
     out = {}
     for u in uniques:
-        orig_mask = orig == u
-        pred_mask = pred == u
-        out[u] = np.sum(pred_mask & orig_mask) / np.sum(orig_mask)
+        om = orig == u
+        pm = pred == u
+        tp = true_positive(pm, om)
+        tn = true_negative(pm, om)
+        fp = false_positive(pm, om)
+        fn = false_negative(pm, om)
+        out[u] = (tp + tn) / (tp + tn + fp + fn)
     return out
 
 
@@ -46,9 +69,11 @@ def precision(pred, orig):
     uniques = set(np.unique(pred)) | set(np.unique(orig))
     out = {}
     for u in uniques:
-        pred_mask = pred == u
-        orig_mask = orig != u
-        out[u] = 1 - np.sum(pred_mask & orig_mask) / np.sum(orig_mask)
+        pm = pred == u
+        om = orig == u
+        tp = true_positive(pm, om)
+        fp = false_positive(pm, om)
+        out[u] = tp / (tp + fp)
     return out
 
 
@@ -57,7 +82,71 @@ def recall(pred, orig):
     uniques = set(np.unique(pred))
     out = {}
     for u in uniques:
-        pred_mask = pred != u
-        orig_mask = orig == u
-        out[u] = 1 - np.sum(pred_mask & orig_mask) / np.sum(orig_mask)
+        pm = pred == u
+        om = orig == u
+        tp = true_positive(pm, om)
+        fn = false_negative(pm, om)
+        out[u] = tp / (tp + fn)
     return out
+
+
+def get_stats(files, model=None, transform_fun=None):
+    if model is not None and not isinstance(model, dict):
+        model = defaultdict(lambda x: model)
+    pred_stats = defaultdict(lambda: defaultdict(dict))
+    bad_files = []
+    for index, file_ in enumerate(files):
+        # if index < 676: continue
+        print(f"{index}: {file_}")
+        data_ = joblib.load(file_)
+        if transform_fun:
+            data_ = transform_fun(data_)
+        data_.get_traced_contours(89.8)
+        n_muscles = len(data_.traced_contours)
+        if model is not None:
+            try:
+                data_.predict(model[n_muscles])
+            except KeyError:
+                bad_files.append(file_)
+                continue
+        data_.get_predicted_contours(89.8)
+
+        if len(data_.traced_contours) != len(data_.predicted_contours):
+            bad_files.append(file_)
+            continue
+        data_.get_contour_areas()
+        data_.get_contour_centers()
+        if n_muscles in {9, 11}:
+            pred_stats[n_muscles][data_.id] = get_stats_(data_)
+        else:
+            bad_files.append(file_)
+    return pred_stats, bad_files
+
+
+def get_stats_(data_):
+    stats_ = {}
+    # area
+    a_ = np.array(data_.predicted_features['area'])
+    b_ = np.array(data_.traced_features['area'])
+    stats_['area_ratio'] = dict(enumerate(a_ / b_))
+    stats_['area_difference'] = dict(enumerate(np.abs(a_ - b_)))
+    # center
+    a_ = np.array(data_.predicted_features['center'])
+    b_ = np.array(data_.traced_features['center'])
+    stats_['center_distance'] = dict(enumerate(np.sqrt(np.sum((a_ - b_) ** 2, axis=1))
+                                                                        * data_.scale))
+    # lever arm
+    a_ = np.array(data_.predicted_features['lever_arm_89.8'])
+    b_ = np.array(data_.traced_features['lever_arm_89.8'])
+    stats_['lever_arm_89.8_distance'] = dict(enumerate(np.abs((a_ - b_))))
+
+    # other
+    a_ = data_.predicted
+    b_ = data_.traced_multilabel_mask
+    stats_['total_accuracy'] = {-1: total_accuracy(a_, b_)}
+    stats_['accuracy'] = accuracy(a_, b_)
+    stats_['precision'] = precision(a_, b_)
+    stats_['recall'] = recall(a_, b_)
+    stats_['has_good_prediction'] = {-1, data_.has_good_prediction()}
+
+    return stats_
